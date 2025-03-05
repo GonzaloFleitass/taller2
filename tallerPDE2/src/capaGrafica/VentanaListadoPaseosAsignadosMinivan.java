00854ed91a845c4ebc7057c6eabc330c05b00048
gonzalofleitas <gonzalofleitas@192.168.1.119>
2025-03-05 20:38:26 -0300

    hola

--- tallerPDE2/src/capaGrafica/VentanaListadoPaseosAsignadosMinivan.java
+++ tallerPDE2/src/capaGrafica/VentanaListadoPaseosAsignadosMinivan.java
@@ -1,83 +1,3 @@
-/* package capaGrafica;
-
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.UIManager;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JList;
-import javax.swing.JTable;
-import java.awt.Font;
-
-public class VentanaListadoPaseosAsignadosMinivan extends JFrame {
-
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField textField;
-	private JTable table;
-
-
-
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaListadoPaseosAsignadosMinivan frame = new VentanaListadoPaseosAsignadosMinivan();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-
-	public VentanaListadoPaseosAsignadosMinivan() {
-		setTitle("LISTADO");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 setBounds(100, 100, 800, 600);
-		contentPane = new JPanel();
-		contentPane.setBackground(UIManager.getColor("List.background"));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		 setLocationRelativeTo(null);
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Matricula");
-		lblNewLabel.setBounds(219, 42, 61, 16);
-		contentPane.add(lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setBounds(310, 37, 130, 26);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JButton btnNewButton = new JButton("Buscar");
-		btnNewButton.setBounds(452, 37, 117, 29);
-		contentPane.add(btnNewButton);
-		
-		JLabel lblNewLabel_1 = new JLabel("Listado de paseos por minivan ");
-		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 18));
-		lblNewLabel_1.setBounds(249, 5, 319, 16);
-		contentPane.add(lblNewLabel_1);
-		
-		JButton Cancelar = new JButton("Cancelar");
-		Cancelar.setBounds(0, 1, 117, 29);
-		contentPane.add(Cancelar);
-		Cancelar.addActionListener(e -> dispose());
-		
-		table = new JTable();
-		table.setBounds(23, 76, 749, 464);
-		contentPane.add(table);
-	}
-
-}
-  */
-
 
 package capaGrafica;
 
@@ -200,7 +120,7 @@ public class VentanaListadoPaseosAsignadosMinivan extends JFrame {
             }
 
             // Obtener la lista de boletos desde el controlador
-            LinkedList<VOPaseo> listaPaseos = controlador.ListarPasPorMini(matricula);
+       
             if (listaPaseos.isEmpty()) {
                 JOptionPane.showMessageDialog(this, "No se encontraron paseos para la matricula ingresada.", 
                         "Información", JOptionPane.INFORMATION_MESSAGE);
@@ -238,7 +158,7 @@ public class VentanaListadoPaseosAsignadosMinivan extends JFrame {
         for (VOPaseo paseo : listaPaseos) {
             model.addRow(new Object[]{
                     paseo.getCodigo(),
-                    paseo.getDestino(),
+                    paseo.getDestino().getNombre(),
                     paseo.getHoraPartida(),
                     paseo.getHoraLlegada(),
                     paseo.getPrecioBase(),
