920777dc64556e4c7db792cda9b8971a0969743d
Gonza <Gonza@DESKTOP-3C407OB>
2025-03-11 15:20:48 -0300

    a

--- tallerPDE2/src/capaGrafica/VentanaAbout.java
+++ tallerPDE2/src/capaGrafica/VentanaAbout.java
@@ -5,6 +5,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.UIManager;
 
 public class VentanaAbout extends JFrame {
 
@@ -34,9 +37,23 @@ public class VentanaAbout extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setForeground(UIManager.getColor("MenuItem.background"));
+		contentPane.setBackground(UIManager.getColor("Menu.selectionForeground"));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Gonza\\Downloads\\Diseño sin título (1).png"));
+		lblNewLabel.setBounds(0, 30, 191, 220);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("<html>Somos un grupo de tres compañeros apasionados por la tecnología. Desarrollamos un software para una empresa de minivanes, con el objetivo de optimizar la gestión, mejorar la logística y ofrecer soluciones personalizadas. Cada uno aportó su experiencia en programación, diseño y análisis de datos, creando una herramienta eficiente y adaptada a la industria del transporte.</html>\n");
+		lblNewLabel_1.setBounds(234, 11, 190, 250);
+		contentPane.add(lblNewLabel_1);
+		
+		
+		
 	}
-
 }
